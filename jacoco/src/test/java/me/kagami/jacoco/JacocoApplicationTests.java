6204f2211699dfe33813f7174f7b50968fb73cc2
tr276913271 <276913271@qq.com>
2018-09-09 10:47:04 +0800

    jacoco

---
+++ jacoco/src/test/java/me/kagami/jacoco/JacocoApplicationTests.java
@@ -0,0 +1,16 @@
+package me.kagami.jacoco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JacocoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ jacoco/src/test/java/me/kagami/jacoco/service/CalculatorTest.java
@@ -0,0 +1,24 @@
+package me.kagami.jacoco.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+	private Calculator instance = new Calculator();
+
+	@Test
+	public void testAdd() {
+		int a = 10;
+		int b = 20;
+		int expected = 30;
+		Assert.assertEquals(expected, instance.add(a, b));
+	}
+
+	@Test
+	public void testSub() {
+		int a = 10;
+		int b = 20;
+		int expected = -10;
+		Assert.assertEquals(expected, instance.sub(a, b));
+	}
+}
